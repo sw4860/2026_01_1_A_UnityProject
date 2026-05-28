4eb5f7181fc630f0fd06a1a061a32044646118b2
sw4860 <parksh070126@gmail.com>
2026-05-28 11:49:18 +0900

    [Feat] 다이얼로그 제작

---
+++ Assets/Scripts/DialogueSystem/DialogueDataSO.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueDataSO", menuName = "Scriptable Objects/DialogueDataSO")]
+public class DialogueDataSO : ScriptableObject
+{
+    [Header("캐릭터 정보")]
+    public string characterName = "캐릭터";
+    public Sprite characterImage;
+
+    [Header("대화 내용")]
+    [TextArea(3, 10)]
+    public List<string> dialogueLines = new();
+}
